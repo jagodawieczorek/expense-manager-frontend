7694155da4f4fff171f7b9f80e05c930755578fe
Jagoda Wieczorek <jagoda.maria.wieczorek@gmail.com>
2022-08-06 23:38:58 +0200

    added Material UI to the project

---
+++ src/components/MyExpenses.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+const MyExpenses = () => {
+    return <Typography variant="h2" component="h1" align="center">My Expenses</Typography>
+}
+
+export default MyExpenses;
