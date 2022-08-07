25045f287e43528cf369084dbf2482a8649dd2a3
Jagoda Wieczorek <jagoda.maria.wieczorek@gmail.com>
2022-08-07 19:40:45 +0200

    show error message in case of not successful response from Expenses API

--- src/components/MyExpenses.tsx
+++ src/components/MyExpenses.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Typography, Alert } from "@mui/material";
 import {useCallback, useEffect, useState } from "react";
 import ExpensesList from "./ExpensesList";
 
@@ -11,14 +11,28 @@ type Expense = {
 
 const MyExpenses: React.FC = () => {
     const [categories, setCategories] = useState<Map<string, number>>(new Map<string, number>());
+    const [errorMessage, setErrorMessage] = useState<string|null>(null);
 
     const fetchCategoriesHandler = useCallback(async () => {
-        const response = await fetch('http://localhost:8080/api/expenses');
-        const data = await response.json();
-        const categoryCountMap: Map<string, number> = data.reduce(
-            (acc: Map<string, number>, expense: Expense) => acc.set(expense.category, (acc.get(expense.category) || 0) + 1),
-              new Map<string, number>());
-        setCategories(categoryCountMap);
+        try {
+            let response = await fetch('http://localhost:8080/api/expenses');
+            if (response.ok) {
+                const data = await response.json();
+                if (data === null) {
+                    throw new Error("You don't have any expenses. Add one!");
+                }
+                const categoryCountMap: Map<string, number> = data.reduce(
+                    (acc: Map<string, number>, expense: Expense) => acc.set(expense.category, (acc.get(expense.category) || 0) + 1),
+                      new Map<string, number>());
+                setCategories(categoryCountMap);
+                setErrorMessage(null);
+            } else {
+                throw new Error("An error occured while fetching categories!");
+            }
+        } catch (error) {
+            setErrorMessage("An error occured while fetching categories!");
+        }
+
     }, []);
 
     useEffect(() => {
@@ -27,7 +41,8 @@ const MyExpenses: React.FC = () => {
 
     return <>
         <Typography variant="h2" component="h1" align="center">My Expenses</Typography>
-        <ExpensesList categories={categories}></ExpensesList>
+        {errorMessage === null && <ExpensesList categories={categories}></ExpensesList>}
+        {errorMessage !== null && <Alert severity="error" >{errorMessage}</Alert>}
     </>;
 }
 
