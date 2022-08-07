57f871dbe92d25fb9d0344d74c752d938a221d68
Jagoda Wieczorek <jagoda.maria.wieczorek@gmail.com>
2022-08-07 02:17:59 +0200

    changed the list to a grid with category items

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,11 +1,22 @@
+import { Typography, Grid, Badge } from "@mui/material";
+import Paper from '@mui/material/Paper';
+
 const ExpensesList: React.FC<{categories: Map<string, number>}> = (props) => {
     const categories = Array.from(props.categories, ([name, count]) => {
         return {['name']: name, ['count']: count};
     });
 
-    return (<ul>
-        {categories.map((category) => <li>Name: {category.name}, count: {category.count}</li>)}
-    </ul>);
+    return (<Grid container spacing={2}>
+        {categories.map((category) => 
+        <Grid item xs={2}>
+            <Paper sx={{height: 200, width: 200}}>
+                <Typography variant="h5" component="h2" align="center">
+                    {category.name}
+                </Typography>
+                <Badge badgeContent={category.count} color="primary"/>
+            </Paper>
+        </Grid>)}
+    </Grid>);
 };
 
 export default ExpensesList;
